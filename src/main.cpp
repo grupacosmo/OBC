66e6e62339b7a9c7b6f9909ab29b3c6d9d8a60d9
Julia Slowikowska <julia.slowikowska@onet.pl>
2021-11-24 19:53:29 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("Hej");
 }
