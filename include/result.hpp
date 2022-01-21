eeb5fa8198ccd41692e1366f635de18dd66bb38d
vixu <lonevixu@gmail.com>
2022-01-21 22:16:16 +0100

    reduced code duplication in Result using delegation to impl

--- include/result.hpp
+++ include/result.hpp
@@ -8,7 +8,8 @@
 
 namespace obc {
 
-struct Unit {};
+struct Unit {
+};
 
 template <typename T>
 struct Ok {
@@ -36,11 +37,9 @@ class Result {
 
    public:
     Result(Ok<T>&& ok) : ok{std::move(ok)}, is_ok_{true} {}
-
     Result(Err<E>&& err) : err{std::move(err)}, is_ok_{false} {}
 
     Result(const Result& other) { construct(other); }
-
     Result(Result&& other) { construct(std::move(other)); }
 
     Result& operator=(const Result& other)
@@ -58,71 +57,32 @@ class Result {
     ~Result()
     {
         if (is_ok()) { ok.~Ok(); }
-        else { err.~Err(); }
+        else {
+            err.~Err();
+        }
     }
 
     bool is_ok() const { return is_ok_; }
-
     bool is_err() const { return !is_ok_; }
 
-    T& unwrap() &
-    {
-        if (OBC_UNLIKELY(is_err())) { OBC_PANIC("unwrap"); }
-        return ok.value;
-    }
+    T& unwrap() & { return unwrap_impl(*this); }
+    T&& unwrap() && { return unwrap_impl(std::move(*this)); }
+    const T& unwrap() const& { return unwrap_impl(*this); }
+    const T&& unwrap() const&& { return unwrap_impl(std::move(*this)); }
 
-    const T& unwrap() const&
-    {
-        if (OBC_UNLIKELY(is_err())) { OBC_PANIC("unwrap"); }
-        return ok.value;
-    }
-
-    T&& unwrap() &&
-    {
-        if (OBC_UNLIKELY(is_err())) { OBC_PANIC("unwrap"); }
-        return std::move(ok.value);
-    }
-    
-    const T&& unwrap() const&&
-    {
-        if (OBC_UNLIKELY(is_err())) { OBC_PANIC("unwrap"); }
-        return std::move(ok.value);
-    }
-
-    E& unwrap_err() &
-    {
-        if (OBC_UNLIKELY(is_ok())) { OBC_PANIC("unwrap_err"); }
-        return err.value;
-    }
-
-    const E& unwrap_err() const&
-    {
-        if (OBC_UNLIKELY(is_ok())) { OBC_PANIC("unwrap_err"); }
-        return err.value;
-    }
-
-    E&& unwrap_err() &&
-    {
-        if (OBC_UNLIKELY(is_ok())) { OBC_PANIC("unwrap_err"); }
-        return std::move(err.value);
-    }
-    
-    const E&& unwrap_err() const&&
-    {
-        if (OBC_UNLIKELY(is_ok())) { OBC_PANIC("unwrap_err"); }
-        return std::move(err.value);
-    }
+    E& unwrap_err() & { return unwrap_err_impl(*this); }
+    E&& unwrap_err() && { return unwrap_err_impl(std::move(*this)); }
+    const E& unwrap_err() const& { return unwrap_err_impl(*this); }
+    const E&& unwrap_err() const&& { return unwrap_err_impl(std::move(*this)); }
 
    private:
     template <typename R>
     void construct(R&& other)
     {
         is_ok_ = other.is_ok_;
-        if (is_ok_) {
-            new(&ok) auto(std::forward<R>(other).ok);
-        }
+        if (is_ok_) { new (&ok) auto(std::forward<R>(other).ok); }
         else {
-            new(&err) auto(std::forward<R>(other).err);
+            new (&err) auto(std::forward<R>(other).err);
         }
     }
 
@@ -133,21 +93,37 @@ class Result {
             if (is_ok_) { ok = std::forward<R>(other).ok; }
             else {
                 err.~Err();
-                new(&ok) auto(std::forward<R>(other).ok);
+                new (&ok) auto(std::forward<R>(other).ok);
             }
         }
         else {
             if (is_ok_) {
                 ok.~Ok();
-                new(&err) auto(std::forward<R>(other).err);
+                new (&err) auto(std::forward<R>(other).err);
             }
-            else { err = std::forward<R>(other).err; }
+            else {
+                err = std::forward<R>(other).err;
+            }
         }
 
         is_ok_ = other.is_ok_;
     }
+
+    template <typename Self>
+    static auto&& unwrap_impl(Self&& self)
+    {
+        if (OBC_UNLIKELY(self.is_err())) { OBC_PANIC("unwrap"); }
+        return std::forward<Self>(self).ok.value;
+    }
+
+    template <typename Self>
+    static auto&& unwrap_err_impl(Self&& self)
+    {
+        if (OBC_UNLIKELY(self.is_ok())) { OBC_PANIC("unwrap_err"); }
+        return std::forward<Self>(self).err.value;
+    }
 };
 
-} // namespace obc
+}  // namespace obc
 
 #endif
