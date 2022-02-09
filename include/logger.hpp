25a529750b35894f54f8c8c046cf32e853535dcc
vixu <lonevixu@gmail.com>
2022-02-09 19:58:58 +0100

    serialization refactoring

--- include/logger.hpp
+++ include/logger.hpp
@@ -24,6 +24,17 @@ String serialize(const Packet &);
 void file_appendln(const char *, const String &);
 void file_appendln(const char *, const char *);
 void date_append(const GpsDate &date);
+void serialize_into(String &buf, const GpsMeasurements &data);
+void serialize_into(String &buf, const BmpMeasurements &data);
+void serialize_into(String &buf, const Acceleration &data);
+void serialize_into(String &buf, const Packet &data);
+template <typename T>
+inline String serialize(const T &t)
+{
+    String buf;
+    serialize_into(buf, t);
+    return buf;
+}
 
 }  // namespace obc
 
--- src/logger.cpp
+++ src/logger.cpp
@@ -11,7 +11,7 @@ unsigned int flight_id = 1;
 
 namespace obc {
 
-Result<Unit, Errc> init(File &boot)
+Result<Unit, Errc> init(File& boot)
 {
     if (!SD.begin(chip_select)) { return Err{Errc::Busy}; }
 
@@ -29,71 +29,77 @@ Result<Unit, Errc> init(File &boot)
     return Ok{Unit{}};
 }
 
-String serialize(const Packet &data)
+void serialize_into(String& buf, const GpsMeasurements& measurements)
 {
-    String logs;
-
-    if (!has_tens_digit(data.gps_measurements.time.hour)) { logs += '0'; }
-    logs += data.gps_measurements.time.hour;
-    logs += ':';
-    if (!has_tens_digit(data.gps_measurements.time.minute)) { logs += '0'; }
-    logs += data.gps_measurements.time.minute;
-    logs += ':';
-    if (!has_tens_digit(data.gps_measurements.time.seconds)) { logs += '0'; }
-    logs += data.gps_measurements.time.seconds;
-    logs += ':';
-    if (!has_hundreds_digit(data.gps_measurements.time.milliseconds)) {
-        if (!has_tens_digit(data.gps_measurements.time.milliseconds)) {
-            logs += "00";
-        }
+    if (!has_tens_digit(measurements.time.hour)) { buf += '0'; }
+    buf += measurements.time.hour;
+    buf += ':';
+    if (!has_tens_digit(measurements.time.minute)) { buf += '0'; }
+    buf += measurements.time.minute;
+    buf += ':';
+    if (!has_tens_digit(measurements.time.seconds)) { buf += '0'; }
+    buf += measurements.time.seconds;
+    buf += ':';
+    if (!has_hundreds_digit(measurements.time.milliseconds)) {
+        if (!has_tens_digit(measurements.time.milliseconds)) { buf += "00"; }
         else {
-            logs += '0';
+            buf += '0';
         }
     }
-    logs += data.gps_measurements.time.milliseconds;
-    logs += "\t";
-
-    logs += "Fix: ";
-    logs += static_cast<int>(data.gps_measurements.position.fix);
-    logs += " quality: ";
-    logs += static_cast<int>(data.gps_measurements.position.fixquality);
-    logs += "\t";
-    if (data.gps_measurements.position.fix) {
-        logs += "Location: ";
-        logs += String(data.gps_measurements.position.latitude, 4);
-        logs += data.gps_measurements.position.lat;
-        logs += ", ";
-        logs += String(data.gps_measurements.position.longitude, 4);
-        logs += data.gps_measurements.position.lon;
-        logs += "\t";
-        logs += "Speed (km/h): ";
-        logs += data.gps_measurements.position.speed / velocity_conversion;
-        logs += "\t";
-        logs += "Altitude: ";
-        logs += data.gps_measurements.position.altitude;
-        logs += "\t";
-        logs += "Satellites: ";
-        logs += static_cast<int>(data.gps_measurements.position.satelites);
+    buf += measurements.time.milliseconds;
+    buf += "\t";
+    buf += "Fix: ";
+    buf += static_cast<int>(measurements.position.fix);
+    buf += " quality: ";
+    buf += static_cast<int>(measurements.position.fixquality);
+    buf += "\t";
+    if (measurements.position.fix) {
+        buf += "Location: ";
+        buf += String(measurements.position.latitude, 4);
+        buf += measurements.position.lat;
+        buf += ", ";
+        buf += String(measurements.position.longitude, 4);
+        buf += measurements.position.lon;
+        buf += "\t";
+        buf += "Speed (km/h): ";
+        buf += measurements.position.speed / velocity_conversion;
+        buf += "\t";
+        buf += "Altitude: ";
+        buf += measurements.position.altitude;
+        buf += "\t";
+        buf += "Satellites: ";
+        buf += static_cast<int>(measurements.position.satelites);
     }
-
-    logs += data.bmp_measurements.temperature;
-    logs += "\t";
-    logs += data.bmp_measurements.pressure;
-    logs += "\t";
-    logs += data.bmp_measurements.altitude;
-    logs += "\t";
-
-    logs += data.acclr_measurements.x;
-    logs += "\t";
-    logs += data.acclr_measurements.y;
-    logs += "\t";
-    logs += data.acclr_measurements.z;
-    logs += "\t";
-
-    return logs;
 }
 
-void date_append(const GpsDate &date)
+void serialize_into(String& buf, const BmpMeasurements& measurements)
+{
+    buf += measurements.temperature;
+    buf += "\t";
+    buf += measurements.pressure;
+    buf += "\t";
+    buf += measurements.altitude;
+    buf += "\t";
+}
+
+void serialize_into(String& buf, const Acceleration& measurements)
+{
+    buf += measurements.x;
+    buf += "\t";
+    buf += measurements.y;
+    buf += "\t";
+    buf += measurements.z;
+    buf += "\t";
+}
+
+void serialize_into(String& buf, const Packet& data)
+{
+    serialize_into(buf, data.gps_measurements);
+    serialize_into(buf, data.bmp_measurements);
+    serialize_into(buf, data.acclr_measurements);
+}
+
+void date_append(const GpsDate& date)
 {
     String date_;
     date_ += "Date: ";
@@ -107,12 +113,12 @@ void date_append(const GpsDate &date)
     file_appendln("/boot.txt", date_);
 }
 
-void file_appendln(const char *file_name, const String &data)
+void file_appendln(const char* file_name, const String& data)
 {
     file_appendln(file_name, data.c_str());
 }
 
-void file_appendln(const char *file_name, const char *data)
+void file_appendln(const char* file_name, const char* data)
 {
     file = SD.open(flight_path_folder + file_name, O_WRITE);
     file.println(data);
